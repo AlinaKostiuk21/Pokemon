e4e2598c576caaa4db38459da291298f072dab36
Alina Kostiuk <alina.hariaha@gmail.com>
2021-12-05 00:35:26 +0200

    Create methods for pokemon description.

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,23 +1,127 @@
 import React from 'react';
-import {SimplePokemon} from "../../react-app-env";
+import {FullPokemon, SimplePokemon} from "../../react-app-env";
 import '../../styles/main.scss';
 import './PokemonCard.scss';
 
 interface Props {
     pokemon: SimplePokemon,
+    fullPokemon?: FullPokemon,
+    onItemClick: (url: string | null) => void,
 }
 
+type KindPokemon = SimplePokemon | FullPokemon;
+
 export const PokemonCard: React.FC<Props> = (props) => {
-    const { pokemon } = props;
+    const { pokemon, fullPokemon, onItemClick } = props;
+
+    const getPokemonImage = (Pokemon: KindPokemon) => {
+        if ("sprites" in Pokemon && Pokemon.sprites) {
+            return Pokemon.sprites.other.dream_world.front_default;
+        }
+
+        return "images/no-pokemon.png"
+    }
+
+    const getPokemonTypes = (aPokemon: KindPokemon) => {
+        if ("types" in aPokemon && aPokemon.types) {
+            return aPokemon.types.map(item => item.type.name);
+        }
+
+        return "???";
+    }
+
+    const getPokemonMoves = (aPokemon: KindPokemon) => {
+        if ("moves" in aPokemon && aPokemon.moves) {
+            return aPokemon.moves.length;
+        }
+
+        return "???";
+    }
+
+    const getPokemonStat = (aPokemon: KindPokemon, stat: string) => {
+        if ("stats" in aPokemon && aPokemon.stats) {
+            const findStat = aPokemon.stats.find(item => item.stat.name === stat);
+            if (findStat) {
+                return findStat.base_stat;
+            }
+        }
+
+        return "???";
+    }
+
+    const aPokemon = fullPokemon || pokemon;
+    const pokemonImage = getPokemonImage(aPokemon);
+    const pokemonTypes = getPokemonTypes(aPokemon);
+    const pokemonMoves = getPokemonMoves(aPokemon);
+    const pokemonHp = getPokemonStat(aPokemon, "hp");
+    const pokemonAttack = getPokemonStat(aPokemon, "attack");
+    const pokemonDefence = getPokemonStat(aPokemon, "defense");
+    const pokemonSpAttack = getPokemonStat(aPokemon, "special-attack");
+    const pokemonSpDefence = getPokemonStat(aPokemon, "special-defense");
+    const pokemonSpeed = getPokemonStat(aPokemon, "speed");
+
 
     return (
-        <div className="pokemon-card">
-            <div className="pokemon-card__image">
-                <img src="" alt="Pokemon"/>
+        <div className="pokemon-card" onClick={() => {
+            if ("url" in aPokemon) {
+                onItemClick(aPokemon.url)
+            } else {
+                onItemClick(null)
+            }
+        }}>
+            <div className="">
+                <img
+                    src={pokemonImage}
+                    alt="Pokemon"
+                    className="pokemon-card__image"
+                />
             </div>
             <div className="pokemon-card__content">
-                <h4 className="pokemon-card__title">{pokemon.name}</h4>
-                <div className="pokemon-card__abilities">About</div>
+                <h4 className="pokemon-card__title">{aPokemon.name}</h4>
+                <div className="pokemon-card__abilities">
+                    <table>
+                        <tr>
+                            <td>Types</td>
+
+                            <td>{typeof pokemonTypes !== "string"
+                                ? pokemonTypes?.map(item => (<button>{item}</button>))
+                                : "???"
+                            }</td>
+                        </tr>
+                    </table>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Weight</td>
+                                <td>{"weight" in aPokemon ? aPokemon.weight : "???"}</td>
+                                <td>Height</td>
+                                <td>{"height" in aPokemon ? aPokemon.height : "???"}</td>
+                            </tr>
+                            <tr>
+                                <td>HP</td>
+                                <td>{pokemonHp}</td>
+                            </tr>
+                            <tr>
+                                <td>Attack</td>
+                                <td>{pokemonAttack}</td>
+                                <td>SP Attack</td>
+                                <td>{pokemonSpAttack}</td>
+                            </tr>
+                            <tr>
+                                <td>Defence</td>
+                                <td>{pokemonDefence}</td>
+                                <td>SP Defence</td>
+                                <td>{pokemonSpDefence}</td>
+                            </tr>
+                            <tr>
+                                <td>Speed</td>
+                                <td>{pokemonSpeed}</td>
+                                <td>Total moves</td>
+                                <td>{pokemonMoves}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
--- src/components/PokemonsList/PokemonsList.tsx
+++ src/components/PokemonsList/PokemonsList.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import {PokemonCard} from "../PokemonCard/PokemonCard";
-import {SimplePokemon} from "../../react-app-env";
+import {FullPokemon, SimplePokemon} from "../../react-app-env";
 import '../../styles/main.scss';
 import './PokemonList.scss';
 
 interface Props {
     pokemons: SimplePokemon[],
+    onItemClick: (url: string | null) => void,
+    fullPokemon: FullPokemon | null,
 }
 
 export const PokemonsList: React.FC<Props> = (props) => {
-    const { pokemons } = props;
+    const { pokemons, onItemClick, fullPokemon } = props;
 
     return (
-        <div className="pokemons grid__wrapper">
-            {pokemons.map(pokemon => (
-                <PokemonCard pokemon={pokemon}/>
-            ))}
+        <div className="pokemons grid__wrapper grid__wrapper--mobile">
+            {pokemons.map(pokemon => {
+                if (fullPokemon && pokemon.name === fullPokemon.name) {
+                    return (
+                        <PokemonCard
+                            pokemon={pokemon}
+                            key={pokemon.name}
+                            onItemClick={onItemClick}
+                            fullPokemon={fullPokemon}
+                        />
+                    )
+                } else {
+                    return (
+                        <PokemonCard
+                            pokemon={pokemon}
+                            key={pokemon.name}
+                            onItemClick={onItemClick}
+                        />
+                    )
+                }
+            })}
         </div>
     );
 };
