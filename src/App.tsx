849adcdc95643b31c73a2abd43b81fd07327ace9
Alina Kostiuk <alina.hariaha@gmail.com>
2021-12-05 00:36:32 +0200

    Create methods for pokemon description.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.scss';
 import './styles/main.scss';
 import {PokemonsList} from "./components/PokemonsList/PokemonsList";
 import {getPokemons, request} from "./api";
-import {SimplePokemon} from "./react-app-env";
+import {FullPokemon, SimplePokemon} from "./react-app-env";
 
 function App() {
     const [pokemons, setPokemons] = useState<SimplePokemon[]>([]);
     const [nextPokemons, setNextPokemons] = useState<string>('');
+    const [selectedPokemon, setSelectedPokemon] = useState<FullPokemon | null>(null);
 
     useEffect(() => {
         getPokemons(0).then(({next, results}) => {
@@ -23,16 +24,29 @@ function App() {
         })
     }
 
+    const selectPokemon = (url: string | null) => {
+        if (url === null) {
+            setSelectedPokemon(null);
+            return;
+        }
+
+        request(url).then((fullPokemon) => {
+            setSelectedPokemon(fullPokemon);
+        })
+    }
+
   return (
     <div className="app">
         <h1 className="app__title">Pokedex</h1>
-        <div className="app__catalog grid">
-            <div className="grid__item--1-6">
+        <div className="app__catalog">
+            <div className="">
                 <PokemonsList
                     pokemons={pokemons}
+                    onItemClick={selectPokemon}
+                    fullPokemon={selectedPokemon}
                 />
             </div>
-            <div className="grid__item--1-6">
+            <div className="">
                 <button
                     className="button-main"
                     onClick={() => loadMore(nextPokemons)}
