b8e439f3fffa92683ea421d5d4adcbfefacee3b7
Alina Kostiuk <alina.hariaha@gmail.com>
2021-12-05 00:32:50 +0200

    Add objects types.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -8,3 +8,45 @@ export interface SimplePokemonsResponse {
     next: string,
     results: SimplePokemon[],
 }
+// types of FullPokemon
+// start "sprites".
+interface OtherSpritesDreamWorldDefault {
+    front_default: string,
+}
+
+interface OtherSpritesDreamWorld {
+    dream_world: OtherSpritesDreamWorldDefault,
+}
+
+interface OtherSprites {
+    other: OtherSpritesDreamWorld,
+}
+// end "sprites".
+// start "types".
+interface PokemonType {
+    name: string,
+}
+
+interface PokemonTypes {
+    type: PokemonType,
+}
+// end "types".
+// start "stats".
+interface PokemonStat {
+    name: string,
+}
+
+interface PokemonStats {
+    base_stat: number,
+    stat: PokemonStat,
+}
+// end "stats".
+export interface FullPokemon {
+    name: string,
+    weight: number,
+    height: number,
+    sprites: OtherSprites,
+    types: PokemonTypes[],
+    moves: any[],
+    stats: PokemonStats[],
+}
